a976d9d874c85c853b7c265ef835e46d4c8493c7
Sebastian <sebastian.mazur.p@gmail.com>
2023-08-24 00:27:02 +0200

    Administrator class

--- src/main/java/Administrator.java
+++ src/main/java/Administrator.java
@@ -1,5 +1,24 @@
 public class Administrator extends  User{
-    public Administrator(String username){
+    public double dietCostPerDay;
+    public double distanceCost;
+    public Administrator(String username,double dietCostPerDay, double distanceCost){
         super(username,"admin");
+        this.dietCostPerDay = dietCostPerDay;
+        this.distanceCost = distanceCost;
+
+        setDietCostPerDay(15);
+        setDistanceCost(0.3);
+    }
+    public double getDietCostPerDay(){
+        return dietCostPerDay;
+    }
+    public double getDistanceCost(){
+        return distanceCost;
+    }
+    public void setDietCostPerDay(double dietCostPerDay){
+        this.distanceCost = dietCostPerDay;
+    }
+    public void setDistanceCost(double distanceCost){
+        this.distanceCost = distanceCost;
     }
 }
